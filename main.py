a54bed8e9bbf6b6367411dde781abfb13904ce47
Amrit <76801862+retrogtx@users.noreply.github.com>
2023-01-13 19:37:47 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+MAX_LINES = 3
+
+
+def deposit():
+    while True:
+        amount = input("what would you like to deposit? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount must be greater than 0.")
+        else:
+            print("Please enter a number.")
+
+    return amount
+
+
+def get_number_of_lines():
+    while True:
+            lines = input("Enter the number of lines to bet on ()")
+            if amount.isdigit():
+                amount = int(amount)
+                if amount > 0:
+                    break
+                else:
+                    print("Amount must be greater than 0.")
+            else:
+                print("Please enter a number.")
+
+        return amount
+
+
+def main():
+    balance = deposit()
+
+
+main()
